a336b3093cb33aad4396f1603f8af7e9664403dd
Jay <jay.ji.jaroen@gmail.com>
2023-10-20 22:09:19 +0700

    initialize the project

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 21:30:52 by jjaroens          #+#    #+#             */
+/*   Updated: 2023/10/20 22:08:57 by jjaroens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifdef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+#endif
